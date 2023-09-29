f9725fe0be26349e683eed82776b3a343ef3b6ab
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-09-29 11:04:19 -0400

    Codigo Airtable

---
+++ Airtable/script.js
@@ -0,0 +1,63 @@
+const apiKey = '';
+const apiUrl = '';
+
+
+fetch(apiUrl, {
+    headers: {
+        Authorization: `Bearer ${apiKey}`
+    }
+})
+.then(response => response.json())
+.then(data => {
+    const products = data.records;
+    const productLista = document.querySelector('.lista-productos');
+
+    let productHTML = ''; // Inicializa una cadena para acumular los productos
+
+    products.forEach((product, index) => {
+        const nombre = product.fields.Nombre;
+        
+        const precio = product.fields.Precio;
+        const descripcion = product.fields.Descripcion;
+        const imagen = product.fields.Imagen;
+
+        if (index % 6 === 0) {
+            if (index !== 0) {
+                // Cierra la fila anterior
+                productHTML += '</div>';
+            }
+            // Abre una nueva fila
+            productHTML += '<div class="row">';
+        }
+
+        // Agrega el producto como una columna
+        productHTML += `
+            <div class="col-lg-2 mb-4">
+                <div class="card">
+                    <img src="${imagen}" class="card-img-top" alt="${nombre}">
+                    <div class="card-body">
+                        <h5 class="card-title">${nombre}</h5>
+                        
+                        <p class="card-text">Precio: ${precio}</p>
+                        <!-- Input para cantidad del producto -->
+                        <div class="input-group mb-3">
+                            <input type="number" class="form-control" placeholder="Cantidad" aria-label="Cantidad" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <!-- Botón dinámico para agregar al carrito -->
+                                <button class="btn btn-primary addToCart">Agregar al Carrito</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    // Cierra la última fila si no es múltiplo de seis
+    if (products.length % 6 !== 0) {
+        productHTML += '</div>';
+    }
+
+    productLista.innerHTML = productHTML;
+})
+.catch(error => console.error(error));
